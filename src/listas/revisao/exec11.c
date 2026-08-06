60eed62cb60cde98f47ce5b0f6a59300afc5de32
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-08-06 09:05:20 -0300

    .

--- src/listas/revisao/exec11.c
+++ src/listas/revisao/exec11.c
@@ -4,7 +4,43 @@ escrevê-la na ordem inversa, senão contar o número de vogais da palavra.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
 int main(){
-	printf("Hello World");
+	char palavra[150];
+	int counter = 0, vowels = 0;
+
+	printf("Digite uma palavra: ");
+	scanf("%s", palavra);
+
+	for (int i = 0; palavra[i] != '\0'; i++){
+		if (palavra[i] == 'A' || palavra[i] == 'E' || palavra[i] == 'I' || palavra[i] == 'O' || palavra[i] == 'U' || palavra[i] == 'a' || palavra[i] == 'e'|| palavra[i] == 'i' || palavra[i] == 'o' || palavra[i] == 'u')
+		{
+			vowels++;
+		}
+		counter++;
+	}
+
+	if (counter % 2 == 0)
+	{
+		printf("Palavra inversa: ");
+		
+		char inverse[150];	
+		int j = 0;
+
+		for (int i = counter - 1; i >= 0; i--)
+		{
+			inverse[j] = palavra[i];
+			j++;
+		}
+		
+		inverse[j] = '\0';
+		
+		printf("%s\n", inverse);
+		
+	} else {
+		printf("A palavra tem %d vogais.", vowels);
+	}
+
 	return 0;
-}
+} 
