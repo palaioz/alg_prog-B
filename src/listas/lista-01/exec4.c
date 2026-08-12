004120f75976cd1f93f9408bde8d55e140267554
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-08-12 09:02:31 -0300

    .

--- src/listas/lista-01/exec4.c
+++ src/listas/lista-01/exec4.c
@@ -5,8 +5,31 @@ Depois de lido o vetor, retorne o nome e o índice do produto de menor preço de
 */
 
 #include <stdio.h>
+#include <string
+
+#define QTD_PRODUTOS 3
+
+struct produtos {
+	char nome[100];
+	float preco_compra;
+	float preco_venda;	
+};
 
 int main(){
-	float produtos[50];
+	struct produtos P[QTD_PRODUTOS];
+	int i;
+
+	for(i = 0; i < QTD_PRODUTOS; i++){
+		printf("\nDigite o nome do produto %d: ", i);
+		fgets(P[i].nome, sizeof(P[i].nome), stdin);
+		P[i].nome[strcspn(P[i].nome, "\n")] = '\0';
+
+		printf("\nDigite o preco de compra: ");
+		scanf("%f", &P[i].preco_compra);
+
+		printf("\nDigite o preco de venda: ");
+		scanf("%f", P[i].preco_venda);
+	}
+
 	return 0;
 }
