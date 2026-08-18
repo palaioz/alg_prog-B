64d7cf187ec304b90a140577884e2a75813e71ad
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2026-08-18 09:44:38 -0300

    .

--- src/listas/lista-01/exec8.c
+++ src/listas/lista-01/exec8.c
@@ -1,5 +1,5 @@
 /*
-8. Escrever um programa que leia duas datas no formato dd/mm/aaaa e verifique qual dada ocorre
+8. Escrever um programa que leia duas datas no formato dd/mm/aaaa e verifique qual data ocorre
 primeiro. Use estruturas para armazenar os dados. Também é necessário o uso da função atoi()
 que converte uma string em um número inteiro.
 */
